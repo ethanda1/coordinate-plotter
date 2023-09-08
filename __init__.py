ccfd887eb01338a9c218cc0580f349f3bf70d134
ethanda1 <savii462@gmail.com>
2023-09-07 23:32:15 -0500

    added label name and title name inputs and also downloadable graph

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session, send_file
 from matplotlib import pyplot as plt
+from flask_session.__init__ import Session
 
 app = Flask(__name__)
-
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 coordinates = []
+xlabel = "x-axis"
+ylabel = "y-axis"
+title = "Coordinate Title"
 
 
 @app.route("/")
@@ -15,7 +20,15 @@ def index():
 def add_coordinate():
     x = float(request.form["x"])
     y = float(request.form["y"])
+    if len(coordinates) == 0:
+        global title
+        title = request.form["coord-name"]
+        global xlabel
+        xlabel = request.form["x-label"]
+        global ylabel
+        ylabel = request.form["y-label"]
     coordinates.append((x, y))
+
     return render_template("index.html", coordinates=coordinates)
 
 
@@ -32,9 +45,9 @@ def plot():
     if len(coordinates) > 0:
         plt.figure(figsize=(8, 6))
         plt.scatter(*zip(*coordinates), marker="o", label="Coordinates")
-        plt.xlabel("X-coordinate")
-        plt.ylabel("Y-coordinate")
-        plt.title("Coordinate Plot")
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
         plt.legend()
         plt.grid(True)
         plt.savefig("static/plot.png")
@@ -47,6 +60,13 @@ def plot():
         )
 
 
+@app.route("/download-plot", methods=["GET"])
+def download_plot():
+    image_path = "static/plot.png"
+    filename = "plot.png"
+    return send_file(image_path, as_attachment=True, download_name=filename)
+
+
 if __name__ == "__main__":
-    app.secret_key = "your_secret_key_here"
+    app.secret_key = "somethingseeecret"
     app.run()
